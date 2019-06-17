5d39bc449957b8fc503f7ba81e1966094291380a
Robin72 <robin.jobidon@free.fr>
2019-06-17 17:02:48 +0200

    Utilisateur fixtures

---
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UtilisateurFixtures extends Fixture
+{
+
+    private $encoder;
+
+    /**
+     * UtilisateurFixtures constructor.
+     * @param $encoder
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+        $admin = new Utilisateur();
+        $admin->setNom("Jobidon");
+        $admin->setPrenom("Robin");
+        $admin->setEmail("robin.jobidon@gmail.com");
+        $admin->setPassword($this->encoder->encodePassword($admin, "1234"));
+        $admin->setRoles(["ROLE_ADMIN"]);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
